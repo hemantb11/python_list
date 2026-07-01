817e7f38a4b51a8cb872d7a29292e4fa952abc86
Hemant Badgujar <hemantbadgujar2006@gmail.com>
2026-06-30 15:40:31 -1200

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,39 @@
+# x=[101,57,6,98,32,57,98]
+# print(x)
+# ip=int(input("Enter your key to find: "))
+# flag=0
+# for i in x:
+#     if ip==i:
+#         print("key found")
+#         flag=1
+# if flag==0:
+#         print("key not found")
+
+
+# duplicate element find in list
+# x=[101,57,6,98,32,57,98]
+# print(x)
+# print("duplicate keys")
+# y=[]
+# for i in x:
+#     if x.count(i)>1 and i not in y:
+#         print(i)
+#         y.append(i)
+
+
+# print unique element
+x=[101,57,6,98,32,57,98]
+print(x)
+print("Unique element: ")
+for i in x:
+    if x.count(i)<=1:
+        print(i)
+
+# print even and odd element
+
+
+
+
+
+
+
